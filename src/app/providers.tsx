a6122f9767739d47e8abbdb055cc80243b3b3a36
narozhin <narozhin@get-lab.ru>
2025-06-21 14:39:14 +0300

    fix: Use HashRouter for GH Pages compatibility

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,10 +1,10 @@
 import type { ReactNode } from 'react';
 import { Provider } from 'react-redux';
 import { store } from './store';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 export const AppProviders = ({ children }: { children: ReactNode }) => (
   <Provider store={store}>
-    <BrowserRouter basename="/SH_frontend">{children}</BrowserRouter>
+    <HashRouter>{children}</HashRouter>
   </Provider>
 );
